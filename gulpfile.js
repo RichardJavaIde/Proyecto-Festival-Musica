92bed852f9ed1b5b535c0a169f258d4aed324db2
unknown <duran41001@gmail.com>
2023-05-03 10:06:02 -0400

    1.1 seccion del festival terminado con las conversiones de imagen webp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const { src, dest, watch } = require("gulp"); // este es para importar funciones de gulp
+const { src, dest, watch, parallel } = require("gulp"); // este es para importar funciones de gulp
+//css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber"); // este plugin es para que no detenga la ejecucion del watch cuando alga un eror y muestre el eror.
+//imagenes
+const webp = require("gulp-webp");
 
 function css(done) {
   src("src/scss/**/*.scss") //indentificar el archivo SASS
@@ -10,10 +13,19 @@ function css(done) {
 
   done(); //Es un Callback que avisa a gulp cuando llegamos al final
 }
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+  done();
+}
 function dev(done) {
   watch("src/scss/**/*.scss", css);
 
   done();
 }
+
 exports.css = css; //es para llamar a ejecutar la funcion
-exports.dev = dev; //es para llamar a ejecutar la funcion
+exports.versionWebp = versionWebp; //es para llamar a ejecutar la funcion
+exports.dev = parallel(versionWebp, dev); //es para llamar a ejecutar la funcion
