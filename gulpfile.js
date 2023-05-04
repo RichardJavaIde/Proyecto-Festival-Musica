25df55eb4540ecc5a7b2ff1f3a4eadd5f25eee89
unknown <duran41001@gmail.com>
2023-05-03 21:29:05 -0400

    1.12 seccion del festival terminado con las conversiones de imagen webp, jpg y avif para que sea mas rapida la pagina.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,10 @@ const { src, dest, watch, parallel } = require("gulp"); // este es para importar
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber"); // este plugin es para que no detenga la ejecucion del watch cuando alga un eror y muestre el eror.
 //imagenes
+const cache = require("gulp-cache");
+const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   src("src/scss/**/*.scss") //indentificar el archivo SASS
@@ -13,11 +16,29 @@ function css(done) {
 
   done(); //Es un Callback que avisa a gulp cuando llegamos al final
 }
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3,
+  };
+  src("src/img/**/*.{png,jpg}") //
+    .pipe(cache(imagemin(opciones))) //
+    .pipe(dest("build/img")); //
+  done();
+}
 function versionWebp(done) {
   const opciones = {
     quality: 50,
   };
   src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+  done();
+}
+function versionavif(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}") //
+    .pipe(avif(opciones)) //
+    .pipe(dest("build/img"));
   done();
 }
 function dev(done) {
@@ -27,5 +48,7 @@ function dev(done) {
 }
 
 exports.css = css; //es para llamar a ejecutar la funcion
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp; //es para llamar a ejecutar la funcion
-exports.dev = parallel(versionWebp, dev); //es para llamar a ejecutar la funcion
+exports.versionavif = versionavif; //es para llamar a ejecutar la funcion
+exports.dev = parallel(versionWebp, versionavif, imagenes, dev); //es para llamar a ejecutar la funcion
