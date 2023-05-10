0b856a0345f6c6e34c77c7e8d6034dec81d567aa
unknown <duran41001@gmail.com>
2023-05-10 10:22:22 -0400

    1.5 La parte de la galeria esta lista.

--- src/js/app.js
+++ src/js/app.js
@@ -22,6 +22,55 @@ function crearGaleria() {
             alt="Imagen galeria."
           />
       `;
+
+    imagen.onclick = function () {
+      mustrarImagen(i);
+    };
     galeria.appendChild(imagen);
   }
+}
+
+function mustrarImagen(id) {
+  const imagen = document.createElement("picture");
+  imagen.innerHTML = `
+      <source srcset="build/img/grande/${id}.avif" type="image/avif" />
+          <source srcset="build/img/grande/${id}.webp" type="image/webp" />
+          <img
+            loading="lazy"
+            width="200"
+            height="300"
+            src="build/img/grande/${id}.jpg"
+            alt="Imagen galeria."
+          />
+      `;
+  // Crea el Overlay con la imagen
+  const overlay = document.createElement("DIV");
+  overlay.appendChild(imagen);
+  overlay.classList.add("overlay");
+
+  overlay.onclick = function () {
+    const body = document.querySelector("body");
+
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+
+  // Boton para cerrar el modal
+  const cerrarModal = document.createElement("P");
+  cerrarModal.textContent = "X";
+  cerrarModal.classList.add("btn-cerrar");
+
+  cerrarModal.onclick = function () {
+    const body = document.querySelector("body");
+
+    body.classList.remove("fijar-body");
+    overlay.remove();
+  };
+
+  overlay.appendChild(cerrarModal);
+
+  // Añadirlo al HTML con el body
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.classList.add("fijar-body");
 }
