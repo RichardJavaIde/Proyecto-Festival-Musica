5500e13e36fff7383b885095628947bc396fc07f
unknown <duran41001@gmail.com>
2023-05-16 09:22:54 -0400

    1.7 agregando mejorar para CSS y JavaScript con la comprecion del codigo.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,28 @@ const { src, dest, watch, parallel } = require("gulp"); // este es para importar
 //css
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber"); // este plugin es para que no detenga la ejecucion del watch cuando alga un eror y muestre el eror.
+
+const autoPrefixer = require("autoprefixer");
+const cssNano = require("cssnano");
+const postCss = require("gulp-postcss");
+const sourceMaps = require("gulp-sourcemaps");
+
 //imagenes
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+//js
+const terser = require("gulp-terser-js");
+
 function css(done) {
   src("src/scss/**/*.scss") //indentificar el archivo SASS
+    .pipe(sourceMaps.init())
     .pipe(plumber())
     .pipe(sass()) //compilarlo
+    .pipe(postCss([autoPrefixer(), cssNano()]))
+    .pipe(sourceMaps.write("."))
     .pipe(dest("build/css")); //almacenarlo en el disco duro
 
   done(); //Es un Callback que avisa a gulp cuando llegamos al final
@@ -43,6 +55,9 @@ function versionavif(done) {
 }
 function javascript(done) {
   src("src/js/**/*.js") //
+    .pipe(sourceMaps.init())
+    .pipe(terser())
+    .pipe(sourceMaps.write("."))
     .pipe(dest("build/js"));
   done();
 }
